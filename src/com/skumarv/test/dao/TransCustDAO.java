6b476cbc3e5d7c327c17458507b0d0e0c640bdbe
Saravanakumar <skumarv@hotmail.com>
2017-09-28 00:20:26 +0530

    useless

--- src/com/skumarv/test/dao/TransCustDAO.java
+++ src/com/skumarv/test/dao/TransCustDAO.java
@@ -10,14 +10,56 @@ import com.skumarv.util.HibernateAnnotationUtil;
 
 public class TransCustDAO {
 	public static void main(String[] args) {
-		Trans trans = new Trans();
+		/*Trans trans = new Trans();
 		Cust cust = new Cust();
 		
 		trans.setAmount(6237.19);
 		cust.setName("Xyz");
 		
 		TransCustDAO obj = new TransCustDAO();
-		obj.insertTranCust(trans, cust);
+		obj.insertTranCust(trans, cust);*/
+		/*Trans trans = new Trans();
+		trans.setAmount(4342.36);
+		TransCustDAO obj = new TransCustDAO();
+		obj.insertTran(trans);
+		trans = new Trans();
+		trans.setAmount(1963.37);
+		obj.insertTran(trans);*/
+		Cust cust = new Cust();
+		cust.setName("Abcds");
+		TransCustDAO obj = new TransCustDAO();
+		obj.updateCust(3, cust);
+	}
+	public void updateCust(int tno, Cust cust) {
+		SessionFactory sf = HibernateAnnotationUtil.getSessionFactory();
+		Session session = sf.openSession();
+		Transaction txn = session.beginTransaction();
+		
+		Trans trans = (Trans) session.get(Trans.class, new Integer(tno));
+		if(trans!=null) {
+			trans.setCust(cust);
+			cust.setTrans(trans);
+			session.persist(trans);
+		}
+		else {
+			System.out.println("Invalid trans");
+		}
+		
+		
+		txn.commit();
+		session.close();
+		
+	}
+	public void insertTran(Trans trans) {
+		SessionFactory sf = HibernateAnnotationUtil.getSessionFactory();
+		Session session = sf.openSession();
+		Transaction txn = session.beginTransaction();
+		
+		session.persist(trans);
+		
+		
+		txn.commit();
+		session.close();
 	}
 	public void insertTranCust(Trans trans, Cust cust) {
 		trans.setCust(cust);
